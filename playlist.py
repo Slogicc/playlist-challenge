d707bfd0742020d391abfb7cd70decf7bd8655a4
Joshua <136170772+Slogicc@users.noreply.github.com>
2026-09-22 10:13:46 -0500

    Added a song
    Added ABC feat. Sophia Black by Polyphia to the song list that is fed into the playlist function

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("ABC feat. Sophia Black", "Polyphia"),
 ]
 
 def print_playlist(songs):
